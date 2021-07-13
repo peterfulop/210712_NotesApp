eef88211029f41df12f576b23eca8e3db1c3d3db
Peter <mrpeterfulop@gmail.com>
2021-07-13 09:56:23 +0200

    07.13. Update

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ import App from './App.js';
 const root = document.getElementById("app");
 const app = new App(root);
 
+/**
+ * Project source:
+ * https://www.youtube.com/watch?v=01YKQmia2Jw&ab_channel=dcode
+ */
+
